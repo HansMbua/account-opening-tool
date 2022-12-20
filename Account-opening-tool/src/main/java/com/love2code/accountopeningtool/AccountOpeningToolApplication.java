4cbebb770254dae28dbaebb740734c4cc53f40ed
hansmbua <hansmbua@yahoo.com>
2022-12-20 04:06:31 +0100

    added spring io

---
+++ Account-opening-tool/src/main/java/com/love2code/accountopeningtool/AccountOpeningToolApplication.java
@@ -0,0 +1,13 @@
+package com.love2code.accountopeningtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountOpeningToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountOpeningToolApplication.class, args);
+    }
+
+}
